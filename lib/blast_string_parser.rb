5be23c4fbdb897d134e8893019bd02c792d8a69e
Philipp Comans <github@volton.otherinbox.com>
2011-04-21 11:55:30 +0200

    Better Regexps

--- lib/blast_string_parser.rb
+++ lib/blast_string_parser.rb
@@ -6,13 +6,13 @@ class BlastStringParser
     
   end
   #Set up Regexps
-  #SPECIES_REGEXP2 = /\A.*\[(\w* \w*).*\].*\z/ #captures the first two words in square brackets
+  #SPECIES_REGEXP2 = /^.*\[(\w* \w*).*\].*$/ #captures the first two words in square brackets
 
-  SPECIES_REGEXP2 = /\A.*\[(.*)\].*\z/ #captures everything in square brackets
+  SPECIES_REGEXP2 = /^.*\[(.*)\].*$/ #captures everything in square brackets
 
-  SGI_REGEXP = /\Agi\|(\d+)\|.*\z/
-  #QUERY_SEQ_REGEXP = /\A([a-zA-Z0-9]+)[_|\s].*\z/ #This captures everything up to the 1st underscore
-  QUERY_SEQ_REGEXP = /\A(\S+)\s.*\z/ #This captures everything until the first whitespace (more robust)
+  SGI_REGEXP = /^gi\|(\d+)\|.*$/
+  #QUERY_SEQ_REGEXP = /^([a-zA-Z0-9]+)[_|\s].*$/ #This captures everything up to the 1st underscore
+  QUERY_SEQ_REGEXP = /^(\S+)\s.*$/ #This captures everything until the first whitespace (more robust)
   #do not expect whitespace after the last | for robustness, strip later
   SUBJ_ANNOTATION_REGEXP = /(?:.*\|)*(.*)\[.*/ #TODO check if this REGEXP captures the right stuff
 
@@ -34,7 +34,7 @@ class BlastStringParser
 
   def get_subject_annotation(a_hit_def)
     unless SUBJ_ANNOTATION_REGEXP.match(a_hit_def)
-      puts "Can not parse subject annotation " + a_hit_def + "\n"
+      puts "Can not parse subject annotation " + a_hit_def[0..20] + "...\n"
       return a_hit_def
     else
       return SUBJ_ANNOTATION_REGEXP.match(a_hit_def)[1].strip
