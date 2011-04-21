61d3bbb2e4b4b4cd7f5c2027d570dffaf7e8c361
Philipp Comans <github@volton.otherinbox.com>
2011-04-21 11:55:53 +0200

    Caching NCBI results

--- lib/ncbi_gi_lookup.rb
+++ lib/ncbi_gi_lookup.rb
@@ -7,10 +7,22 @@ class NCBIGiLookup
       raise "Email address is not a String!"
     end
     @email = email
+    #Set up a cache so we don't have to send multiple queries for the same gi number
+    @cache = {}
   end
+  
   def get_species_name(gi_number)
-    web_result = Bio::NCBI::REST.efetch(gi_number, {"db"=>"protein", "retmode"=>"xml", "email"=>@email})
-    document = Nokogiri::XML(web_result)
-    return document.xpath("//GBSeq_organism").inner_text
+    if @cache.has_key?(gi_number)
+      print "cache hit "
+      return @cache[gi_number]
+    else
+      web_result = Bio::NCBI::REST.efetch(gi_number, {"db"=>"protein", "retmode"=>"xml", "email"=>@email})
+      document = Nokogiri::XML(web_result)
+      species_name = document.xpath("//GBSeq_organism").inner_text
+      
+      #Save species name in cache
+      @cache[gi_number] = species_name
+      return species_name
+    end
   end
 end
