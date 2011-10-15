ec0751a80fdaf6b984e12abd12b61d2e0c582889
Philipp Comans <github@volton.otherinbox.com>
2011-10-16 00:18:20 +0200

    Test with medium sized input files

--- test/test_blackbox.rb
+++ test/test_blackbox.rb
@@ -9,6 +9,7 @@ class BlackBoxTest < Test::Unit::TestCase
   end
   
   def test_small
+    %x[rm test/data/out_3.csv]
     %x[bin/kingdom-assignment -i test/data/in_3.xml -o test/data/out_3.csv]
     result = File.open("test/data/out_3.csv").read
     target = File.open("test/data/target_3.csv").read
@@ -17,7 +18,23 @@ class BlackBoxTest < Test::Unit::TestCase
     assert_not_nil target
 
     assert_equal target, result, "Output of out_3.xml invalid"
-    #%x["rm test/data/out_3.csv"]
+    %x[rm test/data/out_3.csv]
+  end
+
+  def test_medium
+    %x[rm test/data/out_medium.csv]
+    %x[bin/kingdom-assignment -i test/data/in_medium.xml -o test/data/out_medium.csv]
+    result = File.open("test/data/out_medium.csv").read
+    target = File.open("test/data/target_medium.csv").read
+
+    assert_not_nil result
+    assert_not_nil target
+
+    assert_block "Output of out_medium.xml invalid." do
+      result == target
+    end
+    %x[rm test/data/out_medium.csv]
+    
   end
   
 end
